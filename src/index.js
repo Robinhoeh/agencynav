4caf6cebef599c5c344ca572fb61b163a4f9fb31
Robin Watson <drumsrobin@gmail.com>
2017-11-09 14:44:03 -0500

    basic hover and on click done

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import $ from 'jquery';
 
 $(document).ready( () => {
+	$( "a" ).click(function( event ) {
+	  event.preventDefault();
 	$('.hamburger').on('click', () =>{
 		$('.menu').toggleClass('dropDownMenu');
 	});
+});
 });
