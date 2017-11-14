34b67488548e402b52e47bd70731aff9411fcd17
Robin Watson <drumsrobin@gmail.com>
2017-11-13 20:51:25 -0500

    visible changes

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import $ from 'jquery';
 
 $(document).ready( () => {
   $('.hamburger').on('click', () => {
-  $('.dropDownMenu').toggle('dropDownMenu');
+  $('.dropDownMenu').toggleClass('visible');
 	});
 });
 
